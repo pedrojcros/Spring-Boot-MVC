e0ca41dd68b733bc947c4a752777086e9b93d0b3
pjcp0005 <pjcp0005@red.ujaen.es>
2025-05-14 17:24:29 +0200

    adding dependencies, and comment to use context on Main. Deleting applicaiton.propperties

--- ecommerce/src/main/java/app/ecommerce/EcommerceApp.java
+++ ecommerce/src/main/java/app/ecommerce/EcommerceApp.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcommerceApp {
 
 	public static void main(String[] args) {
+		//ConfigurableApplicationContext context =
 		SpringApplication.run(EcommerceApp.class, args);
 	}
 
